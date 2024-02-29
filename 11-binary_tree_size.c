50da90202b1e154ca31e450be731efcae8236758
hebo7865 <lihf95@gmail.com>
2024-02-29 22:18:41 +0200

    tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,8 +3,8 @@
 
 /**
  * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the node to measure the depth
- * Return: tree depth, or 0 if tree is NULL
+ * @tree: pointer to the node to measure the size
+ * Return: tree size, or 0 if tree is NULL
 */
 
 size_t binary_tree_size(const binary_tree_t *tree)
